675a6995effed8f3c94a1e7cba2f1baa4a0be984
prince-conte <prince-conte@outlook.com>
2016-11-12 18:45:09 +0300

    yap

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+
+
+$('.single-item').slick();
+     
+     
+$("a.sc").click(function () {
+var elementClick = $(this).attr("href")
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+return false;
+});
+     
+
+
+});
