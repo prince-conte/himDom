794b597fd97278a9af25eb63fe5afbfd83bfa072
francesco <prince-conte@outlook.com>
2016-11-17 17:57:29 +0300

    yap-2.0-full(responsive+)

--- dist/js/main.js
+++ dist/js/main.js
@@ -117,7 +117,7 @@ $('.sl-t1').slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1100,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
